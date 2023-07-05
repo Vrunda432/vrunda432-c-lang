f539ef8015be0d42e82ba58f82ebdd1edec358ac
vrunda jasani <131241676+Vrunda432@users.noreply.github.com>
2023-07-05 20:47:29 +0530

    Add files via upload

---
+++ modul 3.1/2.oops.c
@@ -0,0 +1,11 @@
+Object-oriented programming (OOP) is a fundamental programming paradigm used by nearly every developer at some point in their career. OOP is the most popular programming paradigm used for software development and is taught as the standard way to code for most of a programmer’s educational career. Another popular programming paradigm is functional programming, but we won’t get into that right now.
+
+Today we will break down the basics of what makes a program object-oriented so that you can start to utilize this paradigm in your algorithms, projects, and interviews.
+types of oops-====>
+
+1.objects
+2.classes
+3.data abstraction and encapsualtion
+4. polymorephisam
+5.inheritance
+6.dyanmaic binding
---
+++ modul 3.1/3.diff oops n pop.cpp
@@ -0,0 +1,53 @@
+1....oops:;;-
+
+=>object orianted.
+
+=>program is divided into objects.
+
+=>bottom-up approch.
+
+=>inheritance property is used.
+
+=>it uses access specifier.
+
+=>encapusuluation is used to hide the data.
+
+=>concept of virtual function.
+
+=>object function are linked through message passing.
+
+=> adding new data and function is easy.
+
+=> the existing code can be reused.
+
+=>use for solving big problems.
+
+ex== c++,java
+
+
+2..pop::-
+
+=> structure oriented.
+
+=>program is divided into functions.
+
+=> top-down apporoch.
+
+=>inheritance is allowed.
+
+=>it doesn't use access specifier.
+
+=>no data hideing.
+
+=>no virtual function.
+
+=>parts of program are linked through parameter passing.
+
+=>expaning new data and function is not easy.
+
+=>no code resubility.
+
+=>not suitable for solving big problems.
+
+ex:- c,pascal
+
